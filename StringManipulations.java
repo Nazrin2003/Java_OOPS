06a13c818e75d116d8ec52f2f615a48c2e6f0fd1
Nazrin K Faizal <nazrinkf@gmail.com>
2025-03-24 23:23:44 +0530

    Program 5

--- StringManipulations.java
+++ StringManipulations.java
@@ -5,18 +5,18 @@ public class StringManipulations {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the first string: ");
-        String str1 = scanner.nextLine(); 
+        String str1 = scanner.nextLine();
 
         System.out.print("Enter the second string: ");
-        String str2 = scanner.nextLine(); 
+        String str2 = scanner.nextLine();
 
         System.out.println("String Manipulations\n---------------------");
-        
-       System.out.println("First String: " + str1+"\nSecond String: " +str2 );
+
+        System.out.println("First String: " + str1 + "\nSecond String: " + str2);
 
         // 1. Concatenation
         String concatanation = str1.concat(" " + str2);
-        System.out.println("Concatenated Strings: " + concatanation );
+        System.out.println("Concatenated Strings: " + concatanation);
 
         // 2. Contains 
         System.out.println("Does " + str1 + " contain 'run'? " + str1.contains("run"));
@@ -28,20 +28,16 @@ public class StringManipulations {
         System.out.println("Are the two strings equal? " + str1.equals(str2));
 
         // 5. Length
-        System.out.println("Length of "+str1+" string: " + str1.length());
+        System.out.println("Length of " + str1 + " string: " + str1.length());
 
         // 6. Replace
-        System.out.println("String '"+str1+"' after replacement: " + str1.replace("run", "spin"));
+        System.out.println("String '" + str1 + "' after replacement: " + str1.replace("run", "spin"));
 
         // 7. Starts With
         System.out.println("Does the first string start with 'run'? " + str1.startsWith("run"));
 
         // 8. Substring
-        if (str1.length() >= 5) {
-            System.out.println("Substring of the first string (0 to 3): " + str1.substring(0, 3));
-        } else {
-            System.out.println("First string is too short for substring (0 to 5).");
-        }
+        System.out.println("Substring of the first string (0 to 3): " + str1.substring(0, 3));
 
         // 9. To Lowercase
         System.out.println("Second string in lowercase: " + str1.toLowerCase());
