155a4a74cb7ad63c19e43e4a4ded7fdbac72e2f2
Nazrin K Faizal <nazrinkf@gmail.com>
2025-04-04 15:13:32 +0530

    Program 15

--- List.java
+++ List.java
@@ -1,32 +1,40 @@
 import java.util.*;  
-class List{  
-public static void main(String args[]){  
-ArrayList<String> l=new ArrayList<String>(); 
-l.add("Ravi");  
-l.add("Vijay");  
-l.add("Ravi");  
-l.add("Ajay");  
-System.out.println("List: " + l);
-//size of list
-System.out.println("Size of the list: " + l.size());
-//remove element
-int i=1;
-l.remove(i);
-System.out.println("Value removed at index "+ i +".Current List: " + l); 
-// //update
-// System.out.print("Enter a string to add: ");
-// String n = sc.nextLine();
-// l.add(n);
-//accessing an element
-int j=0;
-System.out.println("Element at index " + j + ": " + l.get(j));
-//Traversing list through Iterator  
-Iterator itr=l.iterator();  
-while(itr.hasNext()){  
-System.out.println(itr.next());  
-}  
-//clear
-l.clear();
-System.out.println("List cleared. Current List: " + l);
-}  
-}  
+
+class List {  
+    public static void main(String args[]) {  
+        // Create an ArrayList and add elements
+        ArrayList<String> l = new ArrayList<String>(); 
+        l.add("Ravi");  
+        l.add("Vijay");  
+        l.add("Ravi");  
+        l.add("Ajay");  
+        
+        // Display the initial list
+        System.out.println("List: " + l); 
+        
+        // Size of the list
+        System.out.println("Size of the list: " + l.size()); 
+        
+        // Remove element  
+        l.remove(1); 
+        System.out.println("Value removed at index 1. Current List: " + l); 
+        
+        // Update element 
+        l.set(1, "Rahul"); 
+        System.out.println("Updated List after replacing index 1: " + l); 
+        
+        // Contains
+        System.out.println("Contains 'Ravi': " + l.contains("Ravi")); 
+        
+        // Sort the list
+        Collections.sort(l); 
+        System.out.println("Sorted List: " + l); 
+        
+        // Accessing an element
+        System.out.println("Element at index 2: " + l.get(2)); 
+        
+        // Clear the list
+        l.clear(); 
+        System.out.println("List cleared. Current List: " + l); 
+    }  
+}
